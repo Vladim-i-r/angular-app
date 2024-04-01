5b89fbd6088589f3572e2e29c1797d7b920ef84a
Vladimir Lopez <vladimir.luna96@hotmail.com>
2024-03-31 22:20:48 -0600

    Add components and directiva *ngIf, modified CommonModule on App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],   // Faltaba añadir el CommonModule para usar el *ngIf
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-app';
+  title: string = 'Hola Mundo Angular 17';  // No tenia el string, pero con las comillas entiende que es string, antes era title = '';
+  enabled: boolean = false;
+
+  setEnabled(): void{
+    this.enabled = this.enabled? false: true;  // Esto es un if, si es true se cambia a false
+    console.log('Hemos cambiado el title con el click!')
+  }
 }
+
